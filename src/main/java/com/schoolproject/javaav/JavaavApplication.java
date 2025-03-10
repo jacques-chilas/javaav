081b91ef797e6aef9c68d2b1447db9faf9e4be2f
chilas456 <jacqueschilas@gmail.com>
2025-03-10 13:34:36 +0300

    first commit: project creation

---
+++ src/main/java/com/schoolproject/javaav/JavaavApplication.java
@@ -0,0 +1,13 @@
+package com.schoolproject.javaav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolproject/javaav/JavaavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolproject.javaav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
